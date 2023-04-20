d49c43da994b5c724889226183d09e2684d68be3
Brittney Perez <brittney.mp2005@gmail.com>
2023-04-20 02:02:31 -0400

    covered validations & state flow

---
+++ weekThree/D8_forms/Overview.md
@@ -0,0 +1,11 @@
+### ２０２３年０４月１９日（水）
+---
+## **Recap**
+- Front-End Validations (Black Belt Feature 🥷)
+    - attempting to prevent data from passing if invalid
+- Higher Order & Callback Functions | `.map()` and `.filter()`
+
+## **Topics to Cover**
+- Lifting State
+- Flow of State
+- Another Hook! (`useEffect`)
---
+++ weekThree/D8_forms/client/src/App.js
@@ -0,0 +1,23 @@
+import './App.css';
+import React, { useState } from 'react';
+import MovieForm from './components/MovieForm';
+import DisplayAll from './components/DisplayAll';
+import DisplayOne from './components/DisplayOne';
+import UseEffect from './components/UseEffect';
+
+function App() {
+  //        this   &    this...
+  const [ allMovies, setAllMovies ] = useState([]);
+  
+  return (
+    <div className="App">
+      {/*   ...needs to match   this       and            this  */}
+      <MovieForm allMovies={ allMovies } setAllMovies={ setAllMovies } /><hr />
+      <DisplayAll movieList={ allMovies } />
+      <hr /><DisplayOne oneMovie={ allMovies } />
+      <hr /><UseEffect />
+    </div>
+  );
+}
+
+export default App;
