f5a1fa6e3f01adef85eb9fdece1f44d089abed33
Brittney Perez <brittney.mp2005@gmail.com>
2023-05-05 19:38:03 -0400

    finished D14 mat

---
+++ weekFive/D14_Mongoose_II/server/controllers/show.controller.js
@@ -0,0 +1,41 @@
+const Show = require('../models/show.model');
+
+module.exports = {
+    // CREATE
+    createShow: (req, res) => {
+        // console.log(req.body)
+        Show.create(req.body)
+            .then(newShow => res.json(newShow))
+            .catch(err => res.json(err));
+    },
+    
+    // READ ALL
+    getAllShows: (req, res) => {
+        Show.find()
+            .then(allShows => res.json(allShows))
+            .catch(err => res.json(err));
+    },
+    
+    // READ ONE
+    getOneShow: (req, res) => {
+        // console.log(req)
+        Show.findById(req.params.id)
+            .then(oneShow => res.json(oneShow))
+            .catch(err => res.json(err));
+    },
+    
+    // UPDATE
+    // updateShow: (req, res) => {
+    //     Show.findByIdAndUpdate(req.params.id, req.body, { new: true, runValidators: true })
+    //         .then(updatedShow => res.json(updatedShow))
+    //         .catch(err => res.json(err));
+    // },
+
+    // DELETE
+    // deleteShow: (req, res) => {
+    //     Show.findByIdAndDelete(req.params.id)
+    //         .then(result => res.json(result))
+    //         .catch(err => res.json(err));
+    // }
+
+}
---
+++ weekFive/D14_Mongoose_II/server/server.js
@@ -0,0 +1,16 @@
+const express = require('express');
+// const cors = require('cors');
+const app = express();
+// app.use(cors());
+const port = 8000;
+
+require('./config/mongoose.config');
+
+app.use(express.json(), express.urlencoded({ extended: true }));
+
+const AllMyShowRoutes = require('./routes/show.routes');
+AllMyShowRoutes(app);
+
+
+
+app.listen(port, () => console.log(`Listening on port: ${port}`));
